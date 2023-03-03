33582ca149db666840d067acee4c042d3551352b
MarcoManuelJimenezMendez <115948111+MarcoManuelJimenezMendez@users.noreply.github.com>
2023-03-03 10:16:30 -0600

    Add files via upload

---
+++ Casa1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace POOC_Actividad1;
+
+public class Casa1 
+{         
+	//miembros de tipo campo
+        public string nombreDuenoCasa, apellidoDuenoCasa, calleCasa, coloniaCasa, tamanoCasa, ubicacionCasa, materialCasa;
+	    int codigoPostalCasa, puertasCasa, ventanasCasa, numeroHabitacionesCasa;
+		double precioVentaCasa, engancheCasa;
+		ConsoleColor.colorCasa
+		
+		//propiedades
+	    public double EngancheCasa{
+			get {return engancheCasa;}//Obtener
+			set {engancheCasa = value;}//enviar
+			
+		}
+		
+		public double PrecioVentaCasa{
+		 get{return precioVentaCasa;}
+		 set{precioVentaCasa = value;}
+		}
+		
+		public ConsoleColor ColorCasa{
+		get{return colorCasa;}
+		set {colorCasa = value;}
+		
+		
+		}
+	
+		
+		
+	   
+	
+	
+		
+}
--- Program.cs
+++ Program.cs
@@ -1,13 +1,23 @@
 ﻿using System;
 using System.Linq;
 
-namespace POOC_Actividad2;
+namespace POOC_Actividad1;
 
 public static class Program
 {
 	public static void Main()
 	{
+		casa1 objetoCasa1 = new casa1();
+		casa1 objetoCasa2 = new casa1();
+		casa1 objetoCasa3 = new casa1();
+    	casa1 objetoCasa4 = new casa1();
 		
-
+		//Acceder a los miembros de una casa
+		//como? nombreObjeto.accion
+		objetoCasa1.nombreDuenoCasa = "MMJM";
+		objetoCasa2.apellidoDuenoCasa = "Jimenez";
+		
+		
+		
 	}
 }
